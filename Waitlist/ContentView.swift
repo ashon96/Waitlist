0eb23bfe8eaebf646f4ee35af44d27c595e243d3
Andrew <andrew.shon@willowtreeapps.com>
2023-11-02 18:32:04 -0400

    Initial Commit

---
+++ Waitlist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Waitlist
+//
+//  Created by Andrew Shon on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Waitlist/WaitlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaitlistApp.swift
+//  Waitlist
+//
+//  Created by Andrew Shon on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct WaitlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
